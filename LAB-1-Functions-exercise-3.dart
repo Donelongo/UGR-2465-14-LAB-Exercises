a74a2aa83d03e35b63792634320ce272e46662e4
Dagmawi Elias <dagmawielias2@gmail.com>
2024-03-08 16:53:38 +0300

    added function exercise 3

---
+++ LAB-1-Functions-exercise-3.dart
@@ -0,0 +1,16 @@
+//Exercise 3
+
+String reverseString(String input) {
+  String reversed = '';
+  for (int i = input.length - 1; i >= 0; i--) {
+    reversed += input[i];
+  }
+  return reversed;
+}
+
+void main() {
+  String original = 'hello';
+  String reversed = reverseString(original);
+  print('Original string: $original');
+  print('Reversed string: $reversed');
+}
