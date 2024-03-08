2a68bf56f7dcf9316c17985f595853ce74b486a1
Dagmawi Elias <dagmawielias2@gmail.com>
2024-03-08 16:36:47 +0300

    LAB-1-Functions Execrise 1

---
+++ LAB-1-Functions.dart
@@ -0,0 +1,20 @@
+// Exercise 1
+
+import 'dart:io';
+
+double sum(double num1, double num2) {
+  return num1 + num2;
+}
+
+void main() {
+  print("Enter the first number:");
+  String input1 = stdin.readLineSync() ?? "";
+  double number1 = double.tryParse(input1) ?? 0.0;
+
+  print("Enter the second number:");
+  String input2 = stdin.readLineSync() ?? "";
+  double number2 = double.tryParse(input2) ?? 0.0;
+
+  double result = sum(number1, number2);
+  print("The sum of $number1 and $number2 is $result.");
+}
