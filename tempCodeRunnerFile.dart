6b5725fe9e7f7b7faa60195feb4d89a7cd6c8d39
Dagmawi Elias <dagmawielias2@gmail.com>
2024-03-08 16:14:22 +0300

    labLAB-1-Variables-and-types exercise 2

--- tempCodeRunnerFile.dart
+++ tempCodeRunnerFile.dart
@@ -1,15 +1,3 @@
-//Exercise 1
-
-void main() {
-  String name = "Dagmawi Elias";
-  int age = 20;
-  String favoriteColor = "black";
-
-  print("My name is $name, I am $age years old, and my favorite color is $favoriteColor.");
-}
-
-//Exercise 2
-
 import 'dart:io';
 
 void main() {
