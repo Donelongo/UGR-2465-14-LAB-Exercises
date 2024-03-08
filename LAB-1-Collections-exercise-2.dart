46948b77d474e65486420b7216a0255d8c828f0f
Dagmawi Elias <dagmawielias2@gmail.com>
2024-03-08 17:35:49 +0300

    LAB-1-Collections-exercise-2.dart

---
+++ LAB-1-Collections-exercise-2.dart
@@ -0,0 +1,15 @@
+//Exersice 2
+
+import 'dart:math';
+
+void main() {
+  List<int> randomNumbers = List.generate(20, (_) => Random().nextInt(20));
+
+  print("List of random numbers:");
+  print(randomNumbers);
+
+  Set<int> uniqueNumbers = Set<int>.from(randomNumbers);
+
+  print("\nUnique numbers:");
+  print(uniqueNumbers);
+}
