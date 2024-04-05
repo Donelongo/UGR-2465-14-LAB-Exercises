7951516e7906c406abc4d940f7559b70da6862ba
Dagmawi Elias <dagmawielias2@gmail.com>
2024-04-05 16:35:11 +0300

    3rd commit

--- widget_exercise/utracker/lib/bardi.dart
+++ widget_exercise/utracker/lib/bardi.dart
@@ -1,35 +1,71 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    const String title = 'i JTracker';
     return MaterialApp(
-        title: title,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(),
-                    SizedBox(
-                      width: 200,
-                      child: SearchBar(
+      title: 'Flutter Demo',
 
-                        hintText: 'search',
-                      ),
-                    ),
-                    Row(children: [
-                      Icon(Icons.shopping_cart),
-                      Icon(Icons.notifications)
-                    ])
-                  ]),
-            ),
-          ),
-        ));
+      home: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.arrow_back),
+
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+
+                Container(
+                  padding: const EdgeInsets.only(left: 10, right: 150, top: 5, bottom: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.search),
+                      SizedBox(width: 5,),
+                      Text("Search Product")
+                    ],
+                  ),
+
+                ),
+                const SizedBox(width: 25,),
+                const Icon(Icons.shopping_cart),
+                const SizedBox(width: 25,),
+                const Icon(Icons.notifications),
+              ],
+            )
+          ],
+        ),
+
+        body: Column(
+          children: [
+            Image.asset("./assets/coffee.jpg"),
+            const Row(
+              children: [
+                Text(
+                  '\$8.6',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                  Spacer(),
+                  SizedBox(width: 10),
+                  Icon(Icons.favorite),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
