4ec16537efbad47465e9de0f7f3ddfedf9bebf03
Dagmawi Elias <dagmawielias2@gmail.com>
2024-04-05 16:41:40 +0300

    final commit

--- widget_exercise/utracker/lib/bardi.dart
+++ widget_exercise/utracker/lib/bardi.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
               children: [
 
                 Container(
+                  // width: 20,
                   padding: const EdgeInsets.only(left: 10, right: 150, top: 5, bottom: 5),
                   decoration: BoxDecoration(
                     color: Colors.grey.shade100
@@ -35,34 +36,135 @@ class MyApp extends StatelessWidget {
                       Text("Search Product")
                     ],
                   ),
-
                 ),
                 const SizedBox(width: 25,),
                 const Icon(Icons.shopping_cart),
                 const SizedBox(width: 25,),
                 const Icon(Icons.notifications),
+                // SizedBox(width: 150,)
               ],
             )
           ],
         ),
 
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset("./assets/coffee.jpg"),
-            const Row(
-              children: [
-                Text(
-                  '\$8.6',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold
+            Image.asset("../assets/coffee.jpg", height: 400, width: double.infinity, fit: BoxFit.fill,),
+            const SizedBox(height: 20,),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '\$8.6',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                    ),
                   ),
+                  Icon(Icons.favorite)
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text("Bardi Smart Light Bult 120W LED WIFI" , style: TextStyle(fontWeight: FontWeight.w500),),
+                  Text("Bardi Smart Light Bult 120W LED WIFI" ,style: TextStyle(fontWeight: FontWeight.w500),),
+                ],
+              ),
+            ),
+
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.star , color: Colors.yellow,),
+                      SizedBox(width: 2,),
+                      Text("5.0",style: TextStyle(fontWeight: FontWeight.w500)),
+                      SizedBox(width: 2,),
+                      Text("(364)",style: TextStyle(fontWeight: FontWeight.w200)),
+
+                    ],
                   ),
-                  Spacer(),
-                  SizedBox(width: 10),
-                  Icon(Icons.favorite),
-              ],
-            )
+                  SizedBox(width: 20,),
+                  Text("|",style: TextStyle(fontWeight: FontWeight.w200)),
+                  SizedBox(width: 20,),
+                  Text("540 Sale",style: TextStyle(fontWeight: FontWeight.w200)),
+                  SizedBox(width: 20,),
+                  Text("|",style: TextStyle(fontWeight: FontWeight.w200)),
+                  SizedBox(width: 20,),
+                  Row(
+                    children: [
+                      Icon(Icons.location_on_outlined),
+                      SizedBox(width: 2,),
+                      Text("Brooklyn",style: TextStyle(fontWeight: FontWeight.w200)),
+
+                    ],
+                  )
+                ],
+              ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Variant", style: TextStyle(fontWeight: FontWeight.bold),),
+                ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Size: XS", style: TextStyle(fontWeight: FontWeight.bold),),
+                ),
+
+              const Row(
+                children: [
+                  Chip(label: Text("XS"),backgroundColor: Colors.blue),
+                  SizedBox(width: 6,),
+                  Chip(label: Text("XS")),
+                  SizedBox(width: 6,),
+                  Chip(label: Text("XS")),
+                  SizedBox(width: 6,),
+                  Chip(label: Text("XS")),
+                  SizedBox(width: 6,),
+                  Chip(label: Text("XS")),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Color: Red", style: TextStyle(fontWeight: FontWeight.bold),),
+                ),
+
+              const Row(
+                children: [
+                    Chip(label: Text("Red"), backgroundColor: Colors.blue),
+                  SizedBox(width: 6,),
+                  Chip(label: Text("Red")),
+                  SizedBox(width: 6,),
+                  Chip(label: Text("Red")),
+                  SizedBox(width: 6,),
+                  Chip(label: Text("Red")),
+                  SizedBox(width: 6,),
+                  Chip(label: Text("Red")),
+                ],
+              ),
+              const SizedBox(height: 10,),
+              Row(
+                children: [
+                  const Icon(Icons.comment),
+                  const SizedBox(width: 10,),
+                  Expanded(child: Container(
+                    height: 25,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    padding: const EdgeInsets.only(left: 70, right: 70),
+                  child: const Center(child: Text("Add to Shopping Cart"),),) )
+                ],
+              )
           ],
         ),
       ),
